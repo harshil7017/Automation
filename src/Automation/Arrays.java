96ca5ccf567445e5ea2bfed0c6d8f75ca31ae7d4
harshil7017 <harshilsheth@outlook.com>
2022-11-16 17:58:26 +0530

    changes

---
+++ src/Automation/Arrays.java
@@ -0,0 +1,22 @@
+package Automation;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=5;
+		String name="vadodara";
+		char a='r';
+		float dc=4.33f;
+		System.out.println(name + a);
+		
+		int[] arr=new int[5];
+		arr[0]=1;
+		arr[1]=2;
+		arr[2]=4;
+		arr[3]=5;
+		arr[4]=10;
+		
+	}
+
+}
