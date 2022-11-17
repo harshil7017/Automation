9bd6355cddc3d2a8985601bddbd84d742125fbe7
harshil7017 <harshilsheth@outlook.com>
2022-11-17 14:38:11 +0530

    message

---
+++ src/Automation/MethodsDemo.java
@@ -0,0 +1,13 @@
+package Automation;
+
+public class MethodsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public void getdata()
+	{
+		
+	}
+}
--- src/Automation/String1.java
+++ src/Automation/String1.java
@@ -6,12 +6,16 @@ public class String1 {
 
 	public static void main(java.lang.String[] args) {
 		// TODO Auto-generated method stub
-
-		String s1 = new String("HARSHIL SHETH VADODARA");
-		String[] spilttedString=s1.split("harshil");
-		System.out.println(spilttedString[0]);
 		
+		String s1 = new String("HARSHIL SHETH VADODARA");
+		String[] spilttedString=s1.split("VADODARA");
+		System.out.println(spilttedString[0]);
+		for(int i=s1.length()-1;i>=0;i--)
+		{
+			System.out.println(s1.charAt(i));
 				
-	}
+		}
 
+	}
 }
+
