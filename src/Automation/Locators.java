51bf2d8c0ea7ea01f8944023567ce49155ac3d69
harshil7017 <harshilsheth@outlook.com>
2022-11-17 17:22:13 +0530

    locators

---
+++ src/Automation/Locators.java
@@ -0,0 +1,12 @@
+package Automation;
+import BasePackage.Base;
+
+public class Locators extends Base {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		Setup();
+		getLocators();		
+	}
+}
--- src/Automation/MethodsDemo.java
+++ src/Automation/MethodsDemo.java
@@ -4,10 +4,13 @@ public class MethodsDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		MethodsDemo d=new MethodsDemo();
+		String name=d.getdata();
+		System.out.println(name);
 	}
-	public void getdata()
+	public String getdata()
 	{
-		
+		System.out.println("hello");
+		return "harshil";
 	}
 }
