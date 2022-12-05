7c7a2b9d1edfeff50783c2ec7f727a53805bb09c
harshil7017 <harshilsheth@outlook.com>
2022-12-05 14:34:13 +0530

    change

--- src/Automation/ParentChid.java
+++ src/Automation/ParentChid.java
@@ -4,10 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import dev.failsafe.internal.util.Assert;
-
-
-
 public class ParentChid {
 
 	public static void main(String[] args) throws InterruptedException {
