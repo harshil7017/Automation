1fff4616e5164accf763b890f7b3b52de9e0e0ea
harshil7017 <harshilsheth@outlook.com>
2022-12-16 17:55:10 +0530

    change

--- src/Automation/MultiProduct.java
+++ src/Automation/MultiProduct.java
@@ -3,6 +3,7 @@ package Automation;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,19 +12,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MultiProduct {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
 		String[] itemNeeded= {"Cauliflower","Pista"};		
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		List<WebElement> products=driver.findElements(By.cssSelector("h4.product-name"));
 		for(int i=0;i<products.size();i++)
 		{
 			String[] name=products.get(i).getText().split("-");
 			String formattedName=name[0].trim();
 			List itemlist=Arrays.asList(itemNeeded);
-			System.out.println(formattedName);
+			//System.out.println(formattedName);
 	
 			if(itemlist.contains(formattedName))
 			{
@@ -33,7 +35,13 @@ public class MultiProduct {
 			}
 			
 		}
-
+		WebElement cartlist=driver.findElement(By.xpath("//a[@class='cart-icon']"));
+		cartlist.click();
+		driver.findElement(By.xpath("//div[@class='cart-preview active']/div[@class='action-block']")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//input[@class='promoCode']")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.xpath("//button[@class='promoBtn']")).click();
+		System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText());
 	}
 
 }
