4f47718b49e25747d8229246162d32afcf00055b
harshil7017 <harshilsheth@outlook.com>
2022-12-22 16:16:43 +0530

    changes

--- src/Automation/MultiProduct.java
+++ src/Automation/MultiProduct.java
@@ -1,16 +1,13 @@
 package Automation;
 
 
+
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class MultiProduct {
 
@@ -21,14 +18,15 @@ public class MultiProduct {
 		String[] itemNeeded= {"Cauliflower","Pista"};		
 		driver.manage().window().maximize();
 		//driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		//WebDriverWait w = new WebDriverWait(driver, 5);
+		//WebDriverWait w = new WebDriverWait(driver,Duration.ofSeconds(30));
 		//w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.promoInfo")));
 		List<WebElement> products=driver.findElements(By.cssSelector("h4.product-name"));
+	
 		for(int i=0;i<products.size();i++)
 		{
 			String[] name=products.get(i).getText().split("-");
 			String formattedName=name[0].trim();
-			List itemlist=Arrays.asList(itemNeeded);
+			List<String> itemlist=Arrays.asList(itemNeeded);
 			//System.out.println(formattedName);
 	
 			if(itemlist.contains(formattedName))
@@ -46,7 +44,7 @@ public class MultiProduct {
 		driver.findElement(By.xpath("//input[@class='promoCode']")).sendKeys("rahulshettyacademy");
 		driver.findElement(By.xpath("//button[@class='promoBtn']")).click();
 		
-		System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText());
+		
 		
 	}
 
